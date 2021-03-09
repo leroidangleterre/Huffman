3367943e27c669916a1ac15c16be8d59a8b77dcd
Arthur Manoha <arthur.manoha@gmail.com>
2021-03-09 21:02:04 +0100

    Huffman tree structure available

--- Huffman.java
+++ Huffman.java
@@ -2,7 +2,11 @@ package huffman;
 
 import graphs.GraphPanel;
 import graphs.PanelKeyListener;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 /**
@@ -15,7 +19,8 @@ public class Huffman {
 
         JFrame window = new JFrame();
 
-        HuffmanTree root = new HuffmanTree('?');
+        HuffmanTree root = new HuffmanTree();
+        root.setRoot(true);
 
         GraphPanel panel = new GraphPanel(root);
         window.addKeyListener(new PanelKeyListener(root, panel));
@@ -26,8 +31,26 @@ public class Huffman {
         int windowHeight = 600;
         window.setPreferredSize(new Dimension(windowWidth, windowHeight));
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new BorderLayout());
+
+        JButton processButton = new JButton("Process");
+        processButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                root.groupSmallestTrees();
+                panel.repaint();
+            }
+        });
+
+        window.add(processButton, BorderLayout.SOUTH);
         window.pack();
 
+//        root.setText("couco?uaa");
+        root.setText("Coucou les gens, comment ça va, vous allez vraiment bien ?"
+                + " Je suis vraiment content de voir que ça commence à marcher.");
+//        root.setText("Mais si, c’est possible avec la carte Kiwi : enfant de moins de seize ans et ceux qui l’accompagnent jusqu’à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si  c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi  enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Vous savez moi je crois pas qu'il y ait de bonne ou de mauvaise situation… De toute façon c'est la seizième ligne ça devient illisible Mais non c'est pas illisible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possib le Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possib le avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi enfant de moins de seize ans et ceux qui l'accompagnent jusqu'à quatre personnes paient tous moitié prix Moitié prix Mais c’est pas possible Mais si c'est possible avec la carte Kiwi et cette fois c'est fini");
+
         panel.repaint();
+
     }
 }
--- HuffmanTree.java
+++ HuffmanTree.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import static java.lang.Character.isLetter;
+import java.util.ArrayList;
 
 /**
  *
@@ -21,10 +22,13 @@ public class HuffmanTree extends Tree {
     private int fontSize;
 
     private boolean isRoot; // No character is associated with the root.
+    private boolean isCharacter;
 
     public HuffmanTree() {
         this('?');
-        isRoot = true;
+        isRoot = false;
+        isCharacter = false;
+        weight = 0;
     }
 
     public HuffmanTree(char newChar) {
@@ -33,15 +37,27 @@ public class HuffmanTree extends Tree {
         character = newChar;
         remainingText = "";
         weight = 1;
-        if (isLetter(newChar)) {
-            isRoot = false;
-        } else {
-            isRoot = true;
-        }
+        isRoot = false;
+        isCharacter = true;
+    }
+
+    /**
+     * Create a new tree that regroups two smaller trees.
+     * Compute the weight of the new tree as the sum of the two children.
+     *
+     * @param leftChild
+     * @param rightChild
+     */
+    public HuffmanTree(HuffmanTree leftChild, HuffmanTree rightChild) {
+        this();
+        isCharacter = false;
+        branches.add(leftChild);
+        branches.add(rightChild);
     }
 
     public void addChild(HuffmanTree child) {
         branches.add(child);
+        weight += child.weight;
     }
 
     public void setText(String newText) {
@@ -49,9 +65,16 @@ public class HuffmanTree extends Tree {
         while (remainingText.length() > 0) {
             char firstChar = remainingText.charAt(0);
             remainingText = remainingText.substring(1);
-            HuffmanTree newNode = new HuffmanTree(firstChar);
-            branches.add(newNode);
+            HuffmanTree existingNode = findCharacterNode(firstChar);
+            if (existingNode == null) {
+                HuffmanTree newNode = new HuffmanTree(firstChar);
+                branches.add(newNode); // replaced with addChild(newNode);
+            } else {
+                existingNode.weight++;
+            }
+            weight++;
         }
+        sort();
     }
 
     @Override
@@ -64,8 +87,17 @@ public class HuffmanTree extends Tree {
         g.setColor(Color.black);
         g.setFont(new Font(fontName, fontStyle, fontSize));
         int textHeight = g.getFontMetrics().getAscent();
-        int textWidth = g.getFontMetrics().stringWidth(character + "");
-        g.drawString(character + "", (int) (x0 + x * zoom), (int) (y0 + y * zoom + textHeight));
+        int textWidth = getNodeWidth(g);
+        if (isRoot) {
+            // Root of the tree
+            g.drawString("Root", (int) (x0 + x * zoom), (int) (y0 + y * zoom + textHeight));
+        } else if (isCharacter) {
+            // This node represents a character of the original message.
+            g.drawString("" + character, (int) (x0 + x * zoom), (int) (y0 + y * zoom + textHeight));
+        } else {
+            // Non-character node
+            g.drawString(" ", (int) (x0 + x * zoom), (int) (y0 + y * zoom + textHeight));
+        }
         g.drawRect((int) (x0 + x * zoom), (int) (y0 + y * zoom + boxVerticalOffset), textWidth, textHeight);
 
         g.drawString(weight + "", (int) (x0 + x * zoom), (int) (y0 + y * zoom + textHeight + textHeight));
@@ -109,5 +141,78 @@ public class HuffmanTree extends Tree {
     @Override
     public void moveUpOrDown(double dy) {
         super.moveUpOrDown(dy);
+    }
+
+    /**
+     * Find the node that represents a given character.
+     *
+     * @param targetChar the character we are looking for
+     * @return the node that represents @targetChar if it exists, null
+     * otherwise.
+     */
+    private HuffmanTree findCharacterNode(char targetChar) {
+        if (!this.isRoot && this.character == targetChar) {
+            return this;
+        }
+        if (branches == null || branches.isEmpty()) {
+            return null;
+        }
+        for (Tree branch : branches) {
+            HuffmanTree candidate = ((HuffmanTree) branch).findCharacterNode(targetChar);
+            if (candidate != null) {
+                return candidate;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public int getNodeWidth(Graphics g) {
+        return Math.max(g.getFontMetrics().stringWidth(character + ""),
+                g.getFontMetrics().stringWidth(weight + ""));
+    }
+
+    /**
+     * Sort the nodes, with the smallest weight in first position.
+     *
+     */
+    private void sort() {
+        ArrayList<Tree> sortedNodes = new ArrayList<>();
+        for (Tree branch : branches) {
+            sortedNodes.add((HuffmanTree) branch);
+        }
+        sortedNodes.sort((o1, o2) -> {
+            return ((HuffmanTree) o1).weight - ((HuffmanTree) o2).weight;
+        });
+        branches = sortedNodes;
+    }
+
+    /**
+     * Group the smallest two trees as one.
+     * Only if there are at least two trees to group.
+     */
+    protected void groupSmallestTrees() {
+
+        if (branches.size() > 2) {
+            // Step 1: extract the smallest two trees.
+            HuffmanTree smallestOne = (HuffmanTree) branches.remove(0);
+            HuffmanTree smallestTwo = (HuffmanTree) branches.remove(0);
+            // Step 2: group them in a weighted companion tree.
+            HuffmanTree merged = new HuffmanTree(smallestOne, smallestTwo);
+            merged.weight = smallestOne.weight + smallestTwo.weight;
+
+            // Step 3: add the result back to the list and sort the list.
+            branches.add(merged);
+            sort();
+        } else {
+            System.out.println("Nodes already grouped.");
+        }
+    }
+
+    /**
+     * Tell a node that it is the root of a tree (or not).
+     */
+    public void setRoot(boolean b) {
+        isRoot = b;
     }
 }
